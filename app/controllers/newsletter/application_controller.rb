a56b29960d9c2fbf7f062cc3e937c376bbf5f12c
captproton <carl@wdwhub.net>
2022-06-26 12:57:13 -0700

    CHANGE: added layout to application_controller

--- app/controllers/newsletter/application_controller.rb
+++ app/controllers/newsletter/application_controller.rb
@@ -1,4 +1,10 @@
+require 'cancancan'
 module Newsletter
   class ApplicationController < ActionController::Base
+    layout Newsletter.layout
+    load_and_authorize_resource
+
+    helper :'newsletter/layout'
+
   end
 end
